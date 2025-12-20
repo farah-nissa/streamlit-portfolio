45abb0f565131e07a8fa12583420b9392bb2177f
farah-nissa <frhnissa@gmail.com>
2025-12-20 17:29:39 +0700

    Update aboutme.py

--- aboutme.py
+++ aboutme.py
@@ -1,11 +1,15 @@
 import streamlit as st
-import time
 
 def about_me():
     st.subheader('Hi! My name is Farah.')
     st.write("I'm a data scientist. I love building visualizations and making predictions.")
-    st.write('Explore this site!')
-    if st.button('Explore Jakarta Population'):
-        st.experimental_set_query_params(page="Project")
-    if st.button('Explore Jakarta Property Price'):
-        st.experimental_set_query_params(page="Property Price Prediction")
+
+    pages = ["Project", "Property Price Prediction", "Contact"]
+    pill = st.radio("", pages, horizontal=True)
+
+    if pill == "Project":
+        st.switch_page("project.py")
+    elif pill == "Property Price Prediction":
+        st.switch_page("property_price_calculator.py")
+    elif pill == "Contact":
+        st.switch_page("contact.py")
