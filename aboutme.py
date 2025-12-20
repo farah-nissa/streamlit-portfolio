9dae0aa543ddd2c69d724cf77b2da84879722411
farah-nissa <frhnissa@gmail.com>
2025-12-20 17:22:39 +0700

    Update aboutme.py

--- aboutme.py
+++ aboutme.py
@@ -5,5 +5,7 @@ def about_me():
     st.subheader('Hi! My name is Farah.')
     st.write("I'm a data scientist. I love building visualizations and making predictions.")
     st.write('Explore this site!')
-    st.button('Explore Jakarta Population', on_click=lambda: st.experimental_set_query_params(page="Project"))
-    st.button('Explore Jakarta Property Price', on_click=lambda: st.experimental_set_query_params(page="Property Price Prediction"))
+    if st.button('Explore Jakarta Population'):
+        st.experimental_set_query_params(page="Project")
+    if st.button('Explore Jakarta Property Price'):
+        st.experimental_set_query_params(page="Property Price Prediction")
